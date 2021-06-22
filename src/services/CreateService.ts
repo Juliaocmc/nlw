e0fe14a9424cc6a03ab196c12cb1ecb983dd6140
JULIO CESAR MOLINA CORREA <correa_jcm@hotmail.com>
2021-06-22 09:47:11 -0300

    continue 2

---
+++ src/services/CreateService.ts
@@ -0,0 +1,36 @@
+import { UserRepositories } from "../repository/UserRepository"
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+    async execute({ name, email, admin }: IUserRequest) {
+        const userRepository = new UserRepositories();
+
+        if (!email) {
+            throw new Error("E-mail incorrect")
+        }
+
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists")
+        }
+        const user = userRepository.create({
+            name,
+            email,
+            admin
+        })
+
+        await userRepository.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService }
